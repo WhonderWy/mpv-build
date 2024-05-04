774e4bcbd1bd5f3663768004d0fab5594fe73bd0
WhonderWy <26218326+WhonderWy@users.noreply.github.com>
2024-05-05 00:02:37 +1000

    Create helper script

---
+++ archlinux_prepare.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# A file that installs all dependencies I could find to build in an Arch Linux environment.
+
+echo "Updating all packages"
+sudo pacman -Syu
+
+printf "%s\n" --enable-nonfree    >> ffmpeg_options
+printf "%s\n" --enable-libx264    >> ffmpeg_options
+printf "%s\n" --enable-libmp3lame >> ffmpeg_options
+printf "%s\n" --enable-libfdk-aac >> ffmpeg_options
+
+printf "%s\n" -Dlibmpv=true > mpv_options
+
+# git clone https://github.com/KhronosGroup/SPIRV-Cross.git
+
+# cd SPIRV-Cross && mkdir build && cd build && cmake .. && cd .. && make -j4 && cd ..
+
+echo "Downloading libraries"
+pacman_packages=( base-devel cmake wayland meson ffmpeg python-pip python-pipx python-docutils python-pillow python-myst-parser python-pygments ninja yt-dlp libpulse pipewire noto-fonts noto-fonts-cjk noto-fonts-emoji libiconv lua lua52 luajit mujs pkgconfig libbluray libdvdnav libjpeg libarchive unzip libxkbcommon wayland-protocols libiconv smbclient libcdio libdvdnav libdvdread lcms2 rubberband sdl2 vapoursynth zimg jack openal fluidsynth sndio libcaca libdrm libjpeg shaderc libsixel libegl egl-wayland libva libvdpau libcdio libxpresent uchardet )
+sudo pacman -S ${pacman_packages[*]}
+
+echo "Downloading extra dependencies"
+pipx install meson yt-dlp mitmproxy ffmpeg docutils pillow
+
+echo "Building..."
+./rebuild -j4
+
+echo "Installing"
+sudo ./install
